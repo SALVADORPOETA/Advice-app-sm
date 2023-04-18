2610de24a9c31fc7d61c10e26d7b4f341cf8b8c4
SALVADORPOETA <dsml@live.com.mx>
2023-04-17 21:41:17 -0600

    axios

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import axios from 'axios';
 
-import './App.css';
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
 
-class App extends React.Component {
-  state = { advice: '' };
+  useEffect(() => {
+    getQuote();
+  }, []);
 
-  componentDidMount() {
-    this.fetchAdvice();
-  }
+  const getQuote = () => {
+    let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+    axios.get(url)
+      .then(res => {
+        let dataQuotes = res.data.quotes;
+        console.log(dataQuotes);
+        let randomNum = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuote = dataQuotes[randomNum];
 
-  fetchAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice')
-      .then((response) => {
-        const { advice } = response.data.slip;
-
-        this.setState({ advice });
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch(error => console.log(error));
   }
 
-  render() {
-    const { advice } = this.state;
+  const handleClick = () => {
+    getQuote();
+  }
 
-    return (
-      <div className="app">
-        <div className="card">
-          <h1 className="heading">{advice}</h1>
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>GIVE ME ADVICE!</span>
+  return (
+    <div className='app'>
+      <div className="card">
+        <h1 className="heading"><p>{quote}</p></h1>
+        <h2 className="heading">-{author}</h2>
+        <div>
+          <button onClick={handleClick} className="button">
+            <span>GIVE ME INSPIRATION</span>
           </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default App;
